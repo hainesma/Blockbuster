49a12805658dc8f2fa3340bbd8bea06fd99665c6
hainesma <hainesma@gmail.com>
2021-06-08 14:49:33 -0400

    Add project files.

---
+++ Blockbuster/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blockbuster
+{
+    public enum Genre
+    {
+        Drama,
+        Comedy,
+        Horror,
+        Romance,
+        Action
+    }
+    
+    class Movie
+    {
+        public string Title { get; set; }
+        public Genre Category { get; set; }
+        public int RunTime { get; set; }
+        public List<string> Scenes { get; set; }
+
+    }
+}
