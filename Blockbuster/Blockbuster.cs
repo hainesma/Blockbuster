670919b71e98cc3d4d4c6220ae20e95f7607462e
hainesma <hainesma@gmail.com>
2021-06-10 00:20:47 -0400

    PrintMovies

--- Blockbuster/Blockbuster.cs
+++ Blockbuster/Blockbuster.cs
@@ -17,10 +17,12 @@ namespace Blockbuster
             Console.WriteLine("Here is the list of movies");
             for (int i = 0; i < Movies.Count; i++)
             {
-                Console.WriteLine(i + ": " + Movies[i]);
+                Console.WriteLine(i + ": " + Movies[i].Title);
             }
+            Console.WriteLine("Please select a movie by index");
             string input = Console.ReadLine();
             int index = int.Parse(input);
+            Movies[index].PrintInfo();
             return Movies[index];
         }
     }
