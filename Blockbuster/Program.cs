93f2c6991635e7cc08563f053bba47ca9b3d5ea2
hainesma <hainesma@gmail.com>
2021-06-10 09:05:02 -0400

    Added scenes

--- Blockbuster/Program.cs
+++ Blockbuster/Program.cs
@@ -13,12 +13,16 @@ namespace Blockbuster
             List<Movie> offerings = new List<Movie>();
             List<string> scenes1 = new List<string> { "beginning", "middle", "end" };
             List<string> scenes2 = new List<string> { "beginning", "middle", "end" };
+            List<string> scenes3 = new List<string> { "beginning", "middle", "end" };
+            List<string> scenes4 = new List<string> { "beginning", "middle", "end" };
+            List<string> scenes5 = new List<string> { "beginning", "middle", "end" };
+            List<string> scenes6 = new List<string> { "beginning", "middle", "end" };
             VHS v1 = new VHS("After", Genre.Romance, 120, scenes1);
-            VHS v2 = new VHS("1917", Genre.Action, 150, scenes1);
-            VHS v3 = new VHS("Dumb and Dumber", Genre.Comedy, 99, scenes1);
-            DVD d1 = new DVD("Sideways", Genre.Comedy, 135, scenes2);
-            DVD d2 = new DVD("Zoolander", Genre.Comedy, 98, scenes1);
-            DVD d3 = new DVD("High Noon", Genre.Drama, 78, scenes1);
+            VHS v2 = new VHS("1917", Genre.Action, 150, scenes2);
+            VHS v3 = new VHS("Dumb and Dumber", Genre.Comedy, 99, scenes3);
+            DVD d1 = new DVD("Sideways", Genre.Comedy, 135, scenes4);
+            DVD d2 = new DVD("Zoolander", Genre.Comedy, 98, scenes5);
+            DVD d3 = new DVD("High Noon", Genre.Drama, 78, scenes6);
 
             offerings.Add(v1);
             offerings.Add(v2);
